8f2866f39142e908644c7c1cd7ecd4d7cd6c66da
Victor Nicolet <victorn@cs.toronto.edu>
2018-11-12 11:15:02 -0500

    Plot for 64 / 32 cores.

--- plot/plot_exps.py
+++ plot/plot_exps.py
@@ -1,9 +1,15 @@
+import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 
 DATAFILE = '../data/explog_compsbk2.csv'
 
-best_num_threads = [0,1,2,3,4,5,6,7,8,9,10,12,14,16,18,20,24,28,32]#,40,48,52,64]
+MODE_64 = len(sys.argv) > 1
+
+if MODE_64:
+    best_num_threads = [0,1,2,3,4,5,6,7,8,9,10,12,14,16,18,20,24,28,32,40,48,52,64]
+else:
+    best_num_threads = [0,1,2,3,4,5,6,7,8,9,10,12,14,16,18,20,24,28,32]
 
 data = pd.read_csv(DATAFILE, sep=",")
 data = data[data['threads'].isin(best_num_threads)]
@@ -74,9 +80,14 @@ for s, example in examples:
                  linestyle=lex,
                  color=cex, ax=ax)
 
-ax.set_ylim(0, 35)
+if MODE_64:
+    ax.set_ylim(0, 64)
+else:
+    ax.set_ylim(0, 35)
+
 axes_fontsize=20
 legend_fontsize=10
+
 ax.set_ylabel('speedup parallel / sequential',fontsize=axes_fontsize)
 ax.set_xlabel('number of threads', fontsize=axes_fontsize)
 plt.setp(ax.get_xticklabels(), fontsize=axes_fontsize)
